dfaaec63e07b8c9cfc6d7bd50270aee6d8edc8e6
Tobias <mad.scientist@freenet.de>
2016-12-08 23:26:31 +0100

    Changing checkListeners for buttons to the Activity.
    Adding methods for
    - setting the next checked day,
    - checking if alarm time is in the past or future
    - small helper methods
    
    Commented most of what I did. Not sure it is understandable.

--- app/src/main/java/de/bachelorarbeit/duygu/earlybird/AlarmActivity.java
+++ app/src/main/java/de/bachelorarbeit/duygu/earlybird/AlarmActivity.java
@@ -1,7 +1,6 @@
 package de.bachelorarbeit.duygu.earlybird;
 
 import android.annotation.SuppressLint;
-import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -11,7 +10,6 @@ import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Spanned;
 import android.text.method.TimeKeyListener;
 import android.util.Log;
 import android.view.Menu;
@@ -21,25 +19,33 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import de.bachelorarbeit.duygu.earlybird.de.bachelorarbeit.duygu.earlybird.ui.AlarmDate;
 
-import static android.R.id.edit;
-
 /**
  * Created by Duygu on 26.11.2016.
  */
-public class AlarmActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class AlarmActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
+
+    private static final String TAG = AlarmActivity.class.getSimpleName();
+
+    private static final int ERROR_NO_DAY_SET = -1;
+
+    private ToggleButton alarmToggleMO;
+    private ToggleButton alarmToggleDI;
+    private ToggleButton alarmToggleMI;
+    private ToggleButton alarmToggleDO;
+    private ToggleButton alarmToggleFR;
+    private ToggleButton alarmToggleSA;
+    private ToggleButton alarmToggleSO;
+
 
 
     @SuppressLint("StaticFieldLeak")
@@ -93,109 +99,28 @@ public class AlarmActivity extends AppCompatActivity implements AdapterView.OnIt
         // Get the alarm manager service,
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
 
+        // CHANGES FROM HERE....
 
-        final ToggleButton alarmToggleMO = (ToggleButton) findViewById(R.id.toggleButtonMO);
-        final ToggleButton alarmToggleDI = (ToggleButton) findViewById(R.id.toggleButtonDI);
-        final ToggleButton alarmToggleMI = (ToggleButton) findViewById(R.id.toggleButtonMI);
-        final ToggleButton alarmToggleDO = (ToggleButton) findViewById(R.id.toggleButtonDO);
-        final ToggleButton alarmToggleFR = (ToggleButton) findViewById(R.id.toggleButtonFR);
-        final ToggleButton alarmToggleSA = (ToggleButton) findViewById(R.id.toggleButtonSA);
-        final ToggleButton alarmToggleSO = (ToggleButton) findViewById(R.id.toggleButtonSO);
+        // Find all toggle-buttons
+        alarmToggleMO = (ToggleButton) findViewById(R.id.toggleButtonMO);
+        alarmToggleDI = (ToggleButton) findViewById(R.id.toggleButtonDI);
+        alarmToggleMI = (ToggleButton) findViewById(R.id.toggleButtonMI);
+        alarmToggleDO = (ToggleButton) findViewById(R.id.toggleButtonDO);
+        alarmToggleFR = (ToggleButton) findViewById(R.id.toggleButtonFR);
+        alarmToggleSA = (ToggleButton) findViewById(R.id.toggleButtonSA);
+        alarmToggleSO = (ToggleButton) findViewById(R.id.toggleButtonSO);
 
-        alarmToggleMO.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleMO));
-                    if(AlarmDate.getIntCurrentDay()==2) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // set all toggle-buttons to the same listener (this) -> @see onCheckedChanged (line 275)
+        alarmToggleMO.setOnCheckedChangeListener(this);
+        alarmToggleDI.setOnCheckedChangeListener(this);
+        alarmToggleMI.setOnCheckedChangeListener(this);
+        alarmToggleDO.setOnCheckedChangeListener(this);
+        alarmToggleFR.setOnCheckedChangeListener(this);
+        alarmToggleSA.setOnCheckedChangeListener(this);
+        alarmToggleSO.setOnCheckedChangeListener(this);
 
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleDI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleDI));
-                    if(AlarmDate.getIntCurrentDay()==3) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleMI.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleMI));
-                    if(AlarmDate.getIntCurrentDay()==4) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleDO.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleDO));
-                    if(AlarmDate.getIntCurrentDay()==5 ) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleFR.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleFR));
-                    if(AlarmDate.getIntCurrentDay()==6) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleSA.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleSA));
-                    if(AlarmDate.getIntCurrentDay()==7) {
-                        myIntent.putExtra("extra", "yes");
-                        myIntent.putExtra("quote id", String.valueOf(i));
-                        pendingIntent = PendingIntent.getBroadcast(AlarmActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-                    }
-                }
-            }
-        });
-        alarmToggleSO.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @TargetApi(Build.VERSION_CODES.M)
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
+        // THIS WILL BE IN THE NEW METHOD: @see -> setAlarm (line 441)
+                /*if (isChecked) {
                     AlarmDate.setAlarm(alarmTimePicker, calendar, alarmTextView,setStringAlarmDay(alarmToggleSA));
                     if(AlarmDate.getIntCurrentDay()==1) {
                         myIntent.putExtra("extra", "yes");
@@ -206,26 +131,25 @@ public class AlarmActivity extends AppCompatActivity implements AdapterView.OnIt
                     }
                 }
             }
-        });
+        });*/
 
-            Button stop_alarm = (Button) findViewById(R.id.stop_Alarm);
-            stop_alarm.setOnClickListener(new View.OnClickListener(){
-                Context context;
+        Button stop_alarm = (Button) findViewById(R.id.stop_Alarm);
+        stop_alarm.setOnClickListener(new View.OnClickListener(){
+            Context context;
 
-                @Override
-                public void onClick (View v){
-                myIntent.putExtra("extra", "no");
-                myIntent.putExtra("quote id", String.valueOf(i));
-                sendBroadcast(myIntent);
-                alarmManager.cancel(pendingIntent);
-                alarmTextView.setText("Alarm canceled!");
-                alarmManager.cancel(pendingIntent);
-                //setAlarmText("ID is " + i);
-                }
+            @Override
+            public void onClick (View v){
+            myIntent.putExtra("extra", "no");
+            myIntent.putExtra("quote id", String.valueOf(i));
+            sendBroadcast(myIntent);
+            alarmManager.cancel(pendingIntent);
+            alarmTextView.setText("Alarm canceled!");
+            alarmManager.cancel(pendingIntent);
+            //setAlarmText("ID is " + i);
+            }
         });
 
     }
-
 
 
 
@@ -337,5 +261,188 @@ public class AlarmActivity extends AppCompatActivity implements AdapterView.OnIt
         return dayS;
     }
 
+
+
+
+
+    // ---------------------------------------------------------------------------
+    /* ------------------------- TOBI STUFF HERE ------------------------------ */
+    // ---------------------------------------------------------------------------
+
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        /* Called when ever a button got changed.
+        * This will
+        * - remove all alarms and
+        * - find the new next alarm time.
+        * - Then set the alarm to this time and day!
+        *
+        * Steps in detail:
+        * - Determine set alarmTime
+        * - Check if alarmTime is today
+        *   if alarmTime is today -> See if it is in the future!
+        *       if alarmTime is in the future
+        *           setAlarmTo(AlarmTime, today)
+        *       else
+        *           determine next checked day
+        *   else (must be another day)
+        *       determine next checked day
+        *
+        *   setAlarmTo(AlarmTime, daysInFuture)
+        **/
+
+        int dayOfTheWeek = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+        /* Step 1: Prepare hour and minute of the alarm. */
+        int alarmDaysInFuture;  // Delta for "how many days in the future"
+        int alarmHour;          // Hour for the alarm
+        int alarmMinute;        // Minute for the alarm
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // TODO: Set the alarmPicker to a resonable time (e.g. last set alarm)
+            alarmHour = alarmTimePicker.getHour();
+            alarmMinute = alarmTimePicker.getMinute();
+        } else {    // deprecated call for Android API < 23
+            alarmHour = alarmTimePicker.getCurrentHour();
+            alarmMinute = alarmTimePicker.getCurrentMinute();
+        }
+        Log.i(TAG, "Alarm time set to: " + alarmHour + " : " + alarmMinute);
+
+        /* Step 2: Check if today was set, and if not, how many days in the future it is. */
+        if (dayIsChecked(dayOfTheWeek)) {   // Today was checked -> Check if time passed
+            if (! timePassedToday(alarmHour, alarmMinute)) {
+                Log.i(TAG, "Alarm time did not pass today: Set alarm to today");
+                alarmDaysInFuture = 0;
+            } else {
+                alarmDaysInFuture = getDaysToNextCheckedDay();
+                Log.i(TAG, "Alarm time already passed today. Next Alarm is " + alarmDaysInFuture + " days in the future");
+            }
+
+        } else {                            // Today was not checked. See which day is next available
+            alarmDaysInFuture = getDaysToNextCheckedDay();
+            Log.i(TAG, "Alarm is " + alarmDaysInFuture + " days in the future");
+        }
+
+
+        /* Step 3: Notify the user about the next set alarm! */
+        if (alarmDaysInFuture == ERROR_NO_DAY_SET) {    // No day was set
+            Toast.makeText(this, getString(R.string.alarm_set_no_day), Toast.LENGTH_SHORT).show();
+            return;
+
+        } else if (alarmDaysInFuture == 0) {            // Alarm is today
+            Toast.makeText(this,
+                    String.format(getString(R.string.alarm_set_time_today), alarmHour, alarmMinute),
+                    Toast.LENGTH_SHORT).show();
+
+        } else {                                        // Alarm is another day
+            Toast.makeText(this,
+                    String.format(getString(R.string.alarm_set_time_other_day), alarmHour, alarmMinute, alarmDaysInFuture ),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        /* Step 4: Set alarm to time with x days in future */
+        setAlarm(alarmHour, alarmMinute, alarmDaysInFuture);
+    }
+
+    /** Determines whether the current day is checked.
+     *
+     * @return boolean true if the button for today was checked */
+    private boolean dayIsChecked(int dayOfTheWeek){
+        switch (dayOfTheWeek) {
+            case Calendar.MONDAY:                   // #2
+                return alarmToggleMO.isChecked();
+            case Calendar.TUESDAY:                  // #3
+                return alarmToggleDI.isChecked();
+            case Calendar.WEDNESDAY:                // #4
+                return alarmToggleMI.isChecked();
+            case Calendar.THURSDAY:                 // #5
+                return alarmToggleDO.isChecked();
+            case Calendar.FRIDAY:                   // #6
+                return alarmToggleFR.isChecked();
+            case Calendar.SATURDAY:                 // #7
+                return alarmToggleSA.isChecked();
+            case Calendar.SUNDAY:                   // #1 (no index 0 for days!)
+                return alarmToggleSO.isChecked();
+        }
+        Log.w(TAG, "Checking day " + dayOfTheWeek + " is no valid day!");
+        return false;
+    }
+
+    /** Determines whether today already passed a given time (hour and minute)
+     *
+     *  <strong>NOTE: This does not check the day (nor month or year)
+     *  It only tells if a certain hour and minute already passed today. </strong>
+     *
+     * @param hour hour to check
+     * @param minute minute to check
+     * @return boolean true if given time already passed today
+     */
+    private boolean timePassedToday(int hour, int minute){
+        Calendar time = Calendar.getInstance();
+        time.set(Calendar.HOUR_OF_DAY, hour);
+        time.set(Calendar.MINUTE, minute);
+        return time.before(Calendar.getInstance());
+    }
+
+    /** Gets the days until the next selected day.
+     *
+     * Get the current day of the week (as defined in Calendar)
+     * iterate over 7 days (max) in order to find the next day that was checked.
+     *
+     * first day that was checked will be returned.
+     *
+     * @return int amount of days until next selected day
+     */
+    private int getDaysToNextCheckedDay(){
+        int today = Calendar.getInstance().get(Calendar.DAY_OF_WEEK);
+
+        Log.d(TAG, "Today is calendar day " + today);
+
+        /* Starting with tomorrow, we may increment and get the day number calculated % 7 (days of the week).
+        * This gives the option to start over at the first day if we roll over a week.
+        *
+        * e.g. day 8 is the same as day (8 % 7) which is day 1
+        * We also have to check until in 8 days since we start tomorrow
+        * (so in order to get todays date we roll over one week)
+        *
+        * NOTE:
+        * -> Adding +1 to the result is important since the first day of the week starts with index 1
+        * So first day would be day 1 not 0, last day is 7 not 6 (week is 1..7 no 0..6)
+        * -> modulo 7 is required (to start roll over into a new week) so day 8 = day 1 again
+        * -> int i = 1 guarantees starting with the NEXT DAY (since we checked today earlier)
+        */
+        for (int i = 0; i < 7; i++){    // for every day (up to 7 days) starting from tomorrow
+            int checkWeekDay = ((today + i) % 7) + 1;
+            Log.d(TAG, "Checking day " + checkWeekDay  + " (which is in " + (i+1) + " days)");
+
+            if (dayIsChecked(checkWeekDay)){
+                Log.i(TAG, "Next selected week day: " + checkWeekDay);
+                return i + 1;   // Return the first checked day
+            }
+        }
+
+        Log.w(TAG, "No day was selected. Returning " + ERROR_NO_DAY_SET + " in order to signal this error");
+        return ERROR_NO_DAY_SET;
+    }
+
+    /** Sets the alarm for a specified hour and minute at a certain day in the week
+     *
+     * <strong> daysInFuture has to be a positive value in order to set a calendar date in the future.
+     * If the date is the current day, daysInFuture has to be 0.</strong>
+     *
+     * uses: https://developer.android.com/reference/java/util/Calendar.html#roll(int,%20int)
+     *
+     * @param hour hour of the alarm in 24 hour format
+     * @param min minute of the alarm
+     * @param daysInFuture days
+     */
+    private void setAlarm(int hour, int min, int daysInFuture){
+        /* TODO: Set the alarm here.
+        * My first guess would be setting the date plus adding "daysInFuture" days (or x times 24 hours...)
+        * If the day is today these days are 0 so adding this multiplication should never hurt!
+        */
+    }
 }
 
